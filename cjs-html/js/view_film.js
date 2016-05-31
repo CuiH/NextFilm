ed525b77873fe6390db43e078cae695463fdb489
KINSANG CHING <492159680@qq.com>
2016-06-01 00:32:14 +0800

    show-compete!

--- cjs-html/js/view_film.js
+++ cjs-html/js/view_film.js
@@ -22,4 +22,26 @@ $(function(){
 	    }
 	});
 
+	$.ajax({
+		type: "GET",
+	    dataType: "jsonp",
+	    jsonpCallback: "ljy",
+	    url: "http://172.18.43.152:8080/get_showing2?filmId=40",
+	    success: function(data) {
+	        
+	        // 首先判断是否成功
+	        if (data["result"] == "success") {
+	        
+				$("#show-main-container").loadTemplate($("#show-t1"), data["data"]);
+	            
+	        } else {
+	            // DOM 操作
+	            alert("Network error")
+	        }
+	    },
+	    error: function() {
+	        alert("server error1")
+	    }
+	});
+
 })
